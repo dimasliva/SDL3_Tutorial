490df19c7e60324f93fd3a5eaa2e8a6e99bf21d2
Dull <ermilovdima49@gmail.com>
2025-01-30 11:37:06 +0500

    Отрисовка lines, points, rect

--- main.cpp
+++ main.cpp
@@ -10,12 +10,6 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
 	SDL_CreateWindowAndRenderer("SDL3 Game", 800, 640, 0, &window, &renderer);
 	return SDL_APP_CONTINUE;
 }
-SDL_AppResult SDL_AppIterate(void* appstate) {
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-	return SDL_APP_CONTINUE;
-}
 SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
 	if (event->type == SDL_EVENT_QUIT)
 	{
@@ -23,6 +17,43 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
 	}
 	return SDL_APP_CONTINUE;
 }
+SDL_AppResult SDL_AppIterate(void* appstate) {
+	SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+	SDL_RenderClear(renderer);
+
+	SDL_SetRenderDrawColor(renderer, 100, 30, 200, 255);
+	SDL_FRect rect;
+
+	rect.w = 100;
+	rect.h = 100;
+	rect.x = 100;
+	rect.y = 100;
+	SDL_RenderFillRect(renderer, &rect);
+
+	SDL_FRect rectBorder;
+	rectBorder.w = 100;
+	rectBorder.h = 100;
+	rectBorder.x = 300;
+	rectBorder.y = 100;
+	SDL_RenderRect(renderer, &rectBorder);
+
+	SDL_RenderLine(renderer, 450, 100, 500, 200);
+
+	SDL_FPoint lines[] = {
+		{100, 250}, {200, 350}, {300, 400}, {100, 450}
+	};
+	SDL_FPoint points[] = {
+	{200, 350}, {300, 450}, {400, 500}, {200, 550}
+	};
+	SDL_RenderLines(renderer, lines, SDL_arraysize(lines));
+
+	SDL_RenderPoint(renderer, 50, 50);
+	SDL_RenderPoints(renderer, points, SDL_arraysize(points));
+
+	SDL_RenderPresent(renderer);
+	return SDL_APP_CONTINUE;
+}
+
 
 void SDL_AppQuit(void* appstate, SDL_AppResult result) {
 
