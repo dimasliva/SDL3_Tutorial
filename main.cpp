5498641aa58ab7aa614e1790b8e78902307274c0
Dull <ermilovdima49@gmail.com>
2025-02-02 12:53:20 +0500

    Event. Урок 5

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #define SDL_MAIN_USE_CALLBACKS 1
 #include<SDL3/SDL.h>
 #include<SDL3/SDL_main.h>
+#include<iostream>
+#include<stdio.h>
 
 static SDL_Window* window;
 static SDL_Renderer* renderer;
@@ -11,9 +13,51 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
 	return SDL_APP_CONTINUE;
 }
 SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
-	if (event->type == SDL_EVENT_QUIT)
+	switch (event->type)
 	{
+	case SDL_EVENT_QUIT:
 		return SDL_APP_SUCCESS;
+		break;
+	case SDL_EVENT_KEY_DOWN:
+		switch (event->key.scancode)
+		{
+		case SDL_SCANCODE_A:
+			std::cout << "Key pressed A:" << std::endl;
+			break;
+		case SDL_SCANCODE_W:
+			std::cout << "Key pressed W:" << std::endl;
+			break;
+		default:
+			break;
+		}
+		break;
+	case SDL_EVENT_KEY_UP:
+		switch (event->key.scancode)
+		{
+		case SDL_SCANCODE_A:
+			std::cout << "Key unpressed A:" << std::endl;
+			break;
+		case SDL_SCANCODE_W:
+			std::cout << "Key unpressed  W:" << std::endl;
+			break;
+		default:
+			break;
+		}
+		break;
+	case SDL_EVENT_MOUSE_BUTTON_DOWN:
+		printf("Down: %d\n", event->button.button);
+		printf("X: %.2f\n", event->button.x);
+		printf("Y: %.2f\n", event->button.y);
+		std::cout << 1.000001f <<  std::endl;
+		break;
+	case SDL_EVENT_MOUSE_BUTTON_UP:
+		printf("Up: %d\n", event->button.button);
+		printf("X: %.2f\n", event->button.x);
+		printf("Y: %.2f\n", event->button.y);
+		std::cout << 1.000001f << std::endl;
+		break;
+	default:
+		break;
 	}
 	return SDL_APP_CONTINUE;
 }
@@ -21,34 +65,6 @@ SDL_AppResult SDL_AppIterate(void* appstate) {
 	SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
 	SDL_RenderClear(renderer);
 
-	SDL_SetRenderDrawColor(renderer, 100, 30, 200, 255);
-	SDL_FRect rect;
-
-	rect.w = 100;
-	rect.h = 100;
-	rect.x = 100;
-	rect.y = 100;
-	SDL_RenderFillRect(renderer, &rect);
-
-	SDL_FRect rectBorder;
-	rectBorder.w = 100;
-	rectBorder.h = 100;
-	rectBorder.x = 300;
-	rectBorder.y = 100;
-	SDL_RenderRect(renderer, &rectBorder);
-
-	SDL_RenderLine(renderer, 450, 100, 500, 200);
-
-	SDL_FPoint lines[] = {
-		{100, 250}, {200, 350}, {300, 400}, {100, 450}
-	};
-	SDL_FPoint points[] = {
-	{200, 350}, {300, 450}, {400, 500}, {200, 550}
-	};
-	SDL_RenderLines(renderer, lines, SDL_arraysize(lines));
-
-	SDL_RenderPoint(renderer, 50, 50);
-	SDL_RenderPoints(renderer, points, SDL_arraysize(points));
 
 	SDL_RenderPresent(renderer);
 	return SDL_APP_CONTINUE;
