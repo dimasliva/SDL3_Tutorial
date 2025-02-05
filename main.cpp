d9ce4d6e5fd233df0e438c64574a2b24c5277cbb
Dull <ermilovdima49@gmail.com>
2025-02-05 10:50:14 +0500

    Move SDL_FRect. Lesson 6

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 
 static SDL_Window* window;
 static SDL_Renderer* renderer;
+int posX = 0;
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_VIDEO);
@@ -18,44 +19,7 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
 	case SDL_EVENT_QUIT:
 		return SDL_APP_SUCCESS;
 		break;
-	case SDL_EVENT_KEY_DOWN:
-		switch (event->key.scancode)
-		{
-		case SDL_SCANCODE_A:
-			std::cout << "Key pressed A:" << std::endl;
-			break;
-		case SDL_SCANCODE_W:
-			std::cout << "Key pressed W:" << std::endl;
-			break;
-		default:
-			break;
-		}
-		break;
-	case SDL_EVENT_KEY_UP:
-		switch (event->key.scancode)
-		{
-		case SDL_SCANCODE_A:
-			std::cout << "Key unpressed A:" << std::endl;
-			break;
-		case SDL_SCANCODE_W:
-			std::cout << "Key unpressed  W:" << std::endl;
-			break;
-		default:
-			break;
-		}
-		break;
-	case SDL_EVENT_MOUSE_BUTTON_DOWN:
-		printf("Down: %d\n", event->button.button);
-		printf("X: %.2f\n", event->button.x);
-		printf("Y: %.2f\n", event->button.y);
-		std::cout << 1.000001f <<  std::endl;
-		break;
-	case SDL_EVENT_MOUSE_BUTTON_UP:
-		printf("Up: %d\n", event->button.button);
-		printf("X: %.2f\n", event->button.x);
-		printf("Y: %.2f\n", event->button.y);
-		std::cout << 1.000001f << std::endl;
-		break;
+
 	default:
 		break;
 	}
@@ -65,7 +29,18 @@ SDL_AppResult SDL_AppIterate(void* appstate) {
 	SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
 	SDL_RenderClear(renderer);
 
-
+	SDL_SetRenderDrawColor(renderer, 30, 200, 30, 255);
+	posX++;
+	if (posX == 800 - 100) {
+		posX = 0;
+	}
+	SDL_FRect rect;
+	rect.x = posX;
+	rect.y = 0;
+	rect.w = 100;
+	rect.h = 100;
+	SDL_RenderFillRect(renderer, &rect);
+	SDL_Delay(16);
 	SDL_RenderPresent(renderer);
 	return SDL_APP_CONTINUE;
 }
