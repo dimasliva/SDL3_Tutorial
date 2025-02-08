05a08c5a2fd4a95bd7ce62ece937a1171fafe87a
Dull <ermilovdima49@gmail.com>
2025-02-08 11:50:34 +0500

    Lesson 11

--- main.cpp
+++ main.cpp
@@ -5,15 +5,69 @@
 #include<iostream>
 #include<stdio.h>
 
+
+
+
+class Player
+{
+public:
+	Player(SDL_Renderer* renderer);
+	~Player();
+	void handleEvents();
+	void update();
+	void draw();
+
+private:
+	SDL_Renderer* renderer;
+	SDL_FRect src;
+	SDL_FRect dest;
+	SDL_Texture* texture;
+	int speed;
+};
+void Player::draw() {
+	SDL_RenderTexture(renderer, texture, &src, &dest);
+}
+void Player::update() {
+	const bool* keys = SDL_GetKeyboardState(NULL);
+
+	if (keys[SDL_SCANCODE_W]) {
+		dest.y -= speed;
+	}
+	if (keys[SDL_SCANCODE_S]) {
+		dest.y += speed;
+	}
+	if (keys[SDL_SCANCODE_A]) {
+		dest.x -= speed;
+	}
+	if (keys[SDL_SCANCODE_D]) {
+		dest.x += speed;
+	}
+}
+void Player::handleEvents() {
+	
+}
+
+Player::Player(SDL_Renderer* renderer): renderer(renderer)
+{
+	texture = IMG_LoadTexture(renderer, "assets/Prototype_Character/Prototype_Character.png");
+	dest = { 0, 0, 32 * 4, 32 * 4 };
+	src = { 0, 0, 32, 32 };
+	speed = 10;
+}
+
+Player::~Player()
+{
+	SDL_DestroyTexture(texture);
+}
+
 static SDL_Window* window;
 static SDL_Renderer* renderer;
-SDL_FRect rect = { 0, 0, 200, 100 };
-SDL_FRect srcrect = { 200, 200, 100, 100 };
-int speed = 10;
+Player* player = nullptr;
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_CreateWindowAndRenderer("SDL3 Game", 800, 640, 0, &window, &renderer);
+	player = new Player(renderer);
 	return SDL_APP_CONTINUE;
 }
 SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
@@ -26,31 +80,19 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
 	default:
 		break;
 	}
+	player->handleEvents();
 	return SDL_APP_CONTINUE;
 }
 SDL_AppResult SDL_AppIterate(void* appstate) {
 	SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
 	SDL_RenderClear(renderer);
 
-	const bool* keys = SDL_GetKeyboardState(NULL);
+	player->update();
+	player->draw();
 
-	if (keys[SDL_SCANCODE_W]) {
-		rect.y -= speed;
-	}
-	if (keys[SDL_SCANCODE_S]) {
-		rect.y += speed;
-	}
-	if (keys[SDL_SCANCODE_A]) {
-		rect.x -= speed;
-	}
-	if (keys[SDL_SCANCODE_D]) {
-		rect.x += speed;
-	}
 
-	SDL_Texture* playerTexture = IMG_LoadTexture(renderer, "assets/player.png");
-	SDL_GetTextureSize(playerTexture, &rect.w, &rect.h);
-	SDL_RenderTexture(renderer, playerTexture, &srcrect, &rect);
-	SDL_DestroyTexture(playerTexture);
+	
+
 
 	SDL_RenderPresent(renderer);
 	SDL_Delay(16);
